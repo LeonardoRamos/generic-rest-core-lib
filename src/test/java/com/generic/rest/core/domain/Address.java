085ffed4d8ac6e9283db3f74a645e997b12c27e7
Leonardo Ramos <leoslramos@gmail.com>
2023-03-22 16:21:14 -0300

    Refactor

--- src/test/java/com/generic/rest/core/domain/Address.java
+++ src/test/java/com/generic/rest/core/domain/Address.java
@@ -2,7 +2,6 @@ package com.generic.rest.core.domain;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -30,7 +29,7 @@ public class Address extends BaseApiEntity {
 	@JoinColumn(name = "id_country")
 	private Country country;
 	
-	@OneToOne(fetch = FetchType.EAGER)
+	@OneToOne
 	@Cascade(CascadeType.SAVE_UPDATE)
 	@JoinColumn(name = "id_user")
 	@JsonBackReference
