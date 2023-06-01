5ec10316bbc2da2464fe1e0c0a14f573d6151a24
Leonardo Ramos <leoslramos@gmail.com>
2023-06-01 20:48:57 -0300

    Fix loading of tomcat module

--- src/main/java/com/generic/rest/core/GenericRestCoreLibConfiguration.java
+++ src/main/java/com/generic/rest/core/GenericRestCoreLibConfiguration.java
@@ -1,10 +1,13 @@
 package com.generic.rest.core;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
+@AutoConfigureBefore(ServletWebServerFactoryAutoConfiguration.class)
 public class GenericRestCoreLibConfiguration {
 	
 }
